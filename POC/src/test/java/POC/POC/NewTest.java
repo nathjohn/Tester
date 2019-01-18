9dbd8abc4f1bb21ea6247a9802b58216a9fc7b61
NNJN <NNJN@DK01SV1105.scdom.net>
2019-01-18 13:59:39 +0000

    Added f3 to @test

--- POC/src/test/java/POC/POC/NewTest.java
+++ POC/src/test/java/POC/POC/NewTest.java
@@ -37,6 +37,13 @@ public class NewTest {
 			String title = driver.getTitle();				 
 			Assert.assertTrue(title.contains("Google")); 	
 	  }
+	  
+	  @Test
+	  public void f3() {
+			driver.get("https://www.google.com/");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Google")); 	
+	  }
 
 	  @AfterMethod
 	  public void afterMethod() {
